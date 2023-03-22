ada2fde6e1b3fbaf5326838a0f5a638108063974
Jakub Wnęk <127328405+Wneq1@users.noreply.github.com>
2023-03-22 15:14:19 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,24 @@
+ class Main {
+  public static void main(String[] args) {
+
+String[] witaj = new String[11];
+witaj[0] = "h";
+witaj[1] = "e";
+witaj[2] = "l";
+witaj[3] = "l";
+witaj[4] = "o";
+witaj[5] = ".";
+witaj[6] = "w";    
+witaj[7] = "o";    
+witaj[8] = "r";
+witaj[9] = "l";
+witaj[10] = "d";
+    
+  System.out.println("Hello world!");
+  //System.out.println(witaj[1]);
+    for(int i=0; i<11; i++){
+   System.out.println(witaj[i]);
+    
+   }
+  } 
+ }
